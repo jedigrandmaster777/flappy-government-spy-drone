daf9873746d45b44c098587c2a48eda9a47c396c
120958104 <90404123+120958104@users.noreply.github.com>
2022-06-05 09:47:20 -0600

    pausing, and formatting

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const canvas = pjs.createCanvas();
 const score = document.getElementById("score");
 
+let paused = false; //paused starts out as false, to draw the screen at first, and is then set to true
 
 let bird = {
 	real: false,
-  y: 200,
-  motion: 0,
-  score: 0
+ 	y: 200,
+	motion: 0,
+	score: 0
 };
 let pipe = {
 	x: 450,
-  y: 200
+ 	y: 200
 };
 
 pjs.draw = function(){
+	if(paused) return;
+	
 	score.textContent = `Score: ${bird.score}`;
 
 	canvas.background(0, 153, 204);
@@ -25,50 +28,55 @@ pjs.draw = function(){
 	canvas.ctx.drawImage(document.getElementById("pipe"), pipe.x - 55, pipe.y);
 	canvas.ctx.drawImage(document.getElementById("pipeUpsideDown"), pipe.x - 55, pipe.y - 470);
 	//canvas.rect(100 - 10, bird.y - 10, 20, 20);
-  
-  canvas.fill(0, 255, 0);
-  //canvas.rect(pipe.x - 50, pipe.y, 50, 400);
-  //canvas.rect(pipe.x - 50, 0, 50, pipe.y - 100);
-  
-  bird.y += bird.motion;
-  bird.motion++;
-  
-  if(bird.y > 400){
-  	bird = {
+  	canvas.fill(0, 255, 0);
+	//canvas.rect(pipe.x - 50, pipe.y, 50, 400);
+	//canvas.rect(pipe.x - 50, 0, 50, pipe.y - 100);
+	
+	bird.y += bird.motion;
+	bird.motion++;
+	
+	if(bird.y > 400){
+		bird = {
 			real: false,
-  		y: 200,
-  		motion: 0,
-      score: 0
+			y: 200,
+			motion: 0,
+			score: 0
 		};
-    pipe = {
+		pipe = {
 			x: 450,
- 			y: 200
+			y: 200
 		};
-  }
-  
-  pipe.x -= 5;
-  if(pipe.x < 0){
-  	pipe.x = 450;
-    pipe.y = pjs.random(100, 300);
-    bird.score++;
-  }
-  
-  if(!(bird.y + 10 < pipe.y && bird.y - 10 > pipe.y - 100)){
-  	if(100 + 10 > pipe.x - 50 && 100 - 10 < pipe.x){
-    	bird = {
+		paused = true;
+	}
+	pipe.x -= 5;
+	if(pipe.x < 0){
+		pipe.x = 450;
+		pipe.y = pjs.random(100, 300);
+		bird.score++;
+	}
+	
+	if(!(bird.y + 10 < pipe.y && bird.y - 10 > pipe.y - 100)){
+  		if(100 + 10 > pipe.x - 50 && 100 - 10 < pipe.x){
+			bird = {
 				real: false,
-  			y: 200,
-  			motion: 0,
-        score: 0
+				y: 200,
+				motion: 0,
+				score: 0
 			};
 			pipe = {
 				x: 450,
- 				y: 200
+				y: 200
 			};
-    }
-  }
+			paused = true
+		}
+	}
 }
 
+pjs.draw(); //explained above
+paused = true;
+
 pjs.keyPressed = function(e){
+	paused = false;
+	
 	bird.motion = -10;
 }
